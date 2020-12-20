914ea55f599df45a4e51566c6635761fd3371bb5
Felipe Brandao <felipe.brandao97@outlook.com>
2020-12-20 10:24:11 -0300

    Validação sintática com Bean Validation

--- src/main/java/br/com/fb/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/fb/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.fb.cursomc.domain.Categoria;
+import br.com.fb.cursomc.dto.CategoriaDTO;
 import br.com.fb.cursomc.repositories.CategoriaRepository;
 import br.com.fb.cursomc.services.exceptions.DataIntegrityException;
 import br.com.fb.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return rep.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
