179ea3158ada34319200bd1457cfaf0f6420bd09
Felipe Brandao <felipe.brandao97@outlook.com>
2021-01-05 14:31:43 -0300

    Criando profile de desenvolvimento

--- src/main/java/br/com/fb/cursomc/config/DevConfig.java
+++ src/main/java/br/com/fb/cursomc/config/DevConfig.java
@@ -1,6 +1,7 @@
 package br.com.fb.cursomc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,14 +9,22 @@ import org.springframework.context.annotation.Profile;
 import br.com.fb.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
-
+	
+	// pegando o valor da chave que está la no properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateTestDatabase() {
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
