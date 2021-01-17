6a9a8876eaf81c483608e319b0f287f24896e3a9
Felipe Brandao <felipe.brandao97@outlook.com>
2021-01-17 15:49:58 -0300

    Adicionando senha a Cliente

--- src/main/java/br/com/fb/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/br/com/fb/cursomc/dto/ClienteNewDTO.java
@@ -18,35 +18,38 @@ public class ClienteNewDTO implements Serializable {
 	@NotEmpty(message = "Preenchimento obrigatório")
 	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	@Email(message = "Email inválido")
 	private String email;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
-	
+
 	private Integer tipo;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String senha;
 
 	// dados do endereço
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
-	
+
 	private String complemento;
-	
+
 	private String bairro;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
-	
+
 	private String telefone2;
-	
+
 	private String telefone3;
 
 	private Integer cidadeId;
@@ -81,6 +84,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getLogradouro() {
--- src/main/java/br/com/fb/cursomc/services/ClienteService.java
+++ src/main/java/br/com/fb/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +32,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRep;
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = rep.findById(id);
@@ -71,11 +75,11 @@ public class ClienteService {
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
-		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null, null);
 	}
 	
 	public Cliente fromDTO(ClienteNewDTO objDTO) {
-		Cliente cliente = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cliente cliente = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()), encoder.encode(objDTO.getSenha()));
 		Cidade cidade = new Cidade(objDTO.getCidadeId(), null, null);
 		Endereco endereco = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cliente, cidade);
 		
